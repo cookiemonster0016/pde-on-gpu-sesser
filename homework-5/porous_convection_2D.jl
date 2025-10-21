eaee71667271d9863c627f832a1dcaa26e527be6
Sophia Esser <sesser@student.ethz.ch>
2025-10-21 15:47:02 +0200

    verified convergence with 2d -> no good plot though

--- homework-5/porous_convection_2D.jl
+++ homework-5/porous_convection_2D.jl
@@ -1,40 +1,46 @@
 using Plots, Plots.Measures, Printf
 default(size=(1200, 800), framestyle=:box, label=false, grid=false, margin=10mm, lw=6, labelfontsize=20, tickfontsize=20, titlefontsize=24)
 
-@views avx(A) = 0.5 .* (A[1:end-1] .+ A[2:end])#staggered grid averaging
+@views avx(A) = 0.5 .* (A[1:end-1, :] .+ A[2:end,:])#staggered grid averaging x dir
+@views avy(A) = 0.5 .* (A[:, 1:end-1] .+ A[:, 2:end])#staggered grid averaging y dir
 
 @views function porous_convection_2D()
     # physics
-    lx      = 20.0
+    lx      = 40.0
+    ly      = 20.0
     λ       = 0.001
     kvf       = 1.0
     α       = 1.0
     # numerics
     nx      = 100
+    ny = 50
     ϵtol    = 1e-8
-    maxiter = 50nx
-    ncheck  = ceil(Int, 0.25nx)
+    maxiter = 50* max(nx, ny)
+    ncheck  = ceil(Int, 0.25* max(nx, ny))
     nt      = 50
     nvis    = 5
-    r_Pf    = zeros(Float64, nx)
+    r_Pf    = zeros(Float64, nx, ny)
     # derived numerics
     dx      = lx / nx
+    dy = ly / ny
     xc      = LinRange(dx / 2, lx - dx / 2, nx)
+    yc      = LinRange(dy / 2, ly - dy / 2, ny)
     cfl     = 1.0/sqrt(2.1)
     # pressure PT
     re_D    = 2π
-    θ_dτ_D  = lx / re_D / (cfl * dx)
-    β_dτ_D  = kvf * re_D / (cfl * dx * lx)
+    θ_dτ_D  = max(lx, ly) / re_D / (cfl * min(dy, dx))
+    β_dτ_D  = kvf * re_D / (cfl * min(dx , dy) * max(lx, ly))
     # array initialisation
-    # temperature
-    T       = @. exp(-(xc - lx / 4)^2)#at cell centers
-    T_i     = copy(T)
+   
     # pressure
-    P       = zeros(nx)#at cell centers
-    qDx     = zeros(Float64, nx + 1)#flux on faces --> now also exteriour faces
+    P       = zeros(nx, ny)#at cell centers
+    qDx     = zeros(Float64, nx + 1, ny)#flux on faces --> now also exteriour faces
+    qDy     = zeros(Float64, nx, ny + 1)
 
-    #initial condition
-    #? = @. exp(-(xc - lx / 4)^2 - (yc' - ly / 4)^2)
+    #initial condition for Temperature at cell centers
+    T = @. exp(-(xc - lx / 4)^2 - (yc' - ly / 4)^2)
+    T_i     = copy(T)
+
     # time loop
     for it in 1:nt
         @printf("it = %d\n", it)
@@ -42,25 +48,33 @@ default(size=(1200, 800), framestyle=:box, label=false, grid=false, margin=10mm,
         iter = 1; err_Pf = 2ϵtol
         while err_Pf >= ϵtol && iter <= maxiter
             # pressure
-            qDx[2:end-1] .-= (qDx[2:end-1] .+ kvf .* (diff(P) ./ dx .- α .* avx(T))) ./ (θ_dτ_D + 1.0)
+            qDx[2:end-1, :] .-= (qDx[2:end-1, :] .+ kvf .* (diff(P, dims=1) ./ dx .- α .* avx(T))) ./ (θ_dτ_D + 1.0)
+            qDy[:, 2:end-1] .-= (qDy[:, 2:end-1] .+ kvf .* (diff(P, dims=2) ./ dy .- α .* avy(T))) ./ (θ_dτ_D + 1.0)
             #qDx on exteriour faces stays 0
-            P  .-= (diff(qDx) ./ dx) ./ β_dτ_D
+            P  .-= (diff(qDx, dims =1) ./ dx + diff(qDy, dims = 2)./dy) ./ β_dτ_D
 
             if iter % ncheck == 0
-                r_Pf  .= diff(qDx) ./ dx
+                r_Pf  .= diff(qDx, dims=1) ./ dx + diff(qDy, dims=2) ./ dy
                 err_Pf = maximum(abs.(r_Pf))
                 @printf("  iter = %.1f × N, err_Pf = %1.3e\n", iter / nx, err_Pf)
             end
             iter += 1
         end
-        dta = dx / maximum(abs.(qDx)) / 1.1
-        dtd = dx^2 / λ / 2.1
+        dta = min(dy,dx) / max(maximum(abs.(qDx)), maximum(abs.(qDy))) / 1.1
+        dtd = min(dy,dx)^2 / λ / 2.1
         dt  = min(dta, dtd)
         # temperature
-        T[2:end-1] .+= dt .* diff(λ .* diff(T) ./ dx) ./ dx#temperature diffusion
+
+        #temperature diffusion
+        T[2:end-1, :] .+= dt .* diff(λ .* diff(T, dims=1) ./ dx, dims=1) ./ dx
+        T[:, 2:end-1] .+= dt .* diff(λ .* diff(T, dims=2) ./ dy, dims=2) ./ dy
+
         #Temperature advection ->upwind takes the correct direction because of min/max
-        T[2:end-1] .-= dt .* (max.(qDx[3:end-1], 0.0) .* diff(T[1:end-1]) ./ dx .+
-                              min.(qDx[2:end-2], 0.0) .* diff(T[2:end  ]) ./ dx)
+        T[2:end-1, :] .-= dt .* (max.(qDx[3:end-1, :], 0.0) .* diff(T[1:end-1, :], dims = 1) ./ dx .+
+                              min.(qDx[2:end-2, :], 0.0) .* diff(T[2:end, : ], dims = 1) ./ dx)
+
+        T[:, 2:end-1] .-= dt .* (max.(qDy[:, 3:end-1], 0.0) .* diff(T[:, 1:end-1], dims = 2) ./ dy .+
+                              min.(qDy[:, 2:end-2], 0.0) .* diff(T[:, 2:end  ], dims = 2) ./ dy)
 
         if it % nvis == 0
             # visualisation
