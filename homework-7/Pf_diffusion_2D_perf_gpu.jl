b8a6e896863053beb596b66507cbff6eac272abb
Sophia Esser <sesser@student.ethz.ch>
2025-11-06 09:43:16 +0100

    solved Ex2 task2

--- homework-7/Pf_diffusion_2D_perf_gpu.jl
+++ homework-7/Pf_diffusion_2D_perf_gpu.jl
@@ -1,11 +1,10 @@
 
+using Plots, Plots.Measures, Printf, LoopVectorization, BenchmarkTools, CUDA
+default(size=(600, 500), framestyle=:box, label=false, grid=false, margin=10mm, lw=6, labelfontsize=11, tickfontsize=11, titlefontsize=11)
 
-    using Plots, Plots.Measures, Printf, LoopVectorization, BenchmarkTools, CUDA
-    default(size=(600, 500), framestyle=:box, label=false, grid=false, margin=10mm, lw=6, labelfontsize=11, tickfontsize=11, titlefontsize=11)
 
-
-    macro d_xa(A) esc(:( $A[ix+1, iy]-$A[ix,iy]  )) end
-    macro d_ya(A) esc(:( $A[ix, iy+1]-$A[ix,iy]  )) end
+macro d_xa(A) esc(:( $A[ix+1, iy]-$A[ix,iy]  )) end
+macro d_ya(A) esc(:( $A[ix, iy+1]-$A[ix,iy]  )) end
 
 
     #CPU Functions =#
@@ -38,9 +37,14 @@ end
     #= GPU Kernels =#
 function update_Pf!(nx, ny, Pf, qDx, qDy, _β_dτ_dx, _β_dτ_dy)
 
+    ix = (blockIdx().x-1) * blockDim().x + threadIdx().x
+    iy = (blockIdx().y-1) * blockDim().y + threadIdx().y
+
     if ix >= 1 && iy >=1 && ix <= nx && iy <= ny
-    
-        @inbounds Pf[ix, iy] -= (@d_xa(qDx)) * _β_dτ_dx  +  (@d_ya(qDy)) * _β_dτ_dy
+        
+        @inbounds begin
+            Pf[ix, iy] -= (qDx[ix+1, iy] - qDx[ix, iy]) * _β_dτ_dx  +  (qDy[ix, iy+1] - qDy[ix, iy]) * _β_dτ_dy
+        end
     end
 
     return nothing
@@ -53,11 +57,11 @@ function compute_flux!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ)
 
     if iy >=1 && iy <= ny && ix >=1 && ix <= nx-1
 
-        @inbounds qDx[ix+1, iy] -= (qDx[ix+1, iy] + k_ηf_dx *@d_xa(Pf)) * _1_θ_dτ 
+        @inbounds qDx[ix+1, iy] -= (qDx[ix+1, iy] + k_ηf_dx *(Pf[ix+1, iy] - Pf[ix, iy])) * _1_θ_dτ 
     end
 
     if iy >=1 && iy <= ny-1 && ix >=1 && ix <= nx  
-        @inbounds qDy[ix, iy+1] -= (qDy[ix, iy+1] + k_ηf_dy *@d_ya(Pf)) * _1_θ_dτ
+        @inbounds qDy[ix, iy+1] -= (qDy[ix, iy+1] + k_ηf_dy *(Pf[ix, iy+1] - Pf[ix, iy])) * _1_θ_dτ
     end
 
 
@@ -67,15 +71,22 @@ end
 
    
 
-function compute!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ, _β_dτ_dx, _β_dτ_dy, useThreads)
-    if useThreads 
-        compute_flux_t!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ)
-        update_Pf_t!(nx, ny, Pf, qDx, qDy, _β_dτ_dx, _β_dτ_dy)
-    else
-        compute_flux!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ)
-        update_Pf!(nx, ny, Pf, qDx, qDy, _β_dτ_dx, _β_dτ_dy)
-    end
+function compute!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ, _β_dτ_dx, _β_dτ_dy, threads, blocks)
+
+    @cuda threads=threads blocks=blocks compute_flux!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ)
+    synchronize()
+    @cuda threads=threads blocks=blocks update_Pf!(nx, ny, Pf, qDx, qDy, _β_dτ_dx, _β_dτ_dy)
+    synchronize()
+    return nothing
 end
+
+function compute_t!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ, _β_dτ_dx, _β_dτ_dy)
+
+    compute_flux_t!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ)
+    update_Pf_t!(nx, ny, Pf, qDx, qDy, _β_dτ_dx, _β_dτ_dy)
+    return nothing
+end
+
 
 
 function Pf_diffusion_2D(nx, ny , maxiter;do_check = false, useThreads = false)
@@ -88,21 +99,32 @@ function Pf_diffusion_2D(nx, ny , maxiter;do_check = false, useThreads = false)
     cfl     = 1.0 / sqrt(2.1)
     re      = 2π
     threads = (32, 4)
-    blocks = (nx/threads[1], ny/threads[2])
+    blocks = blocks  = (nx ÷ threads[1], ny ÷ threads[2])
     #fill up nx and ny to be multiples of threads
-    nx = threads[1]*blocks[1]
-    ny = threads[2]*blocks[2]
+    nx = Int(threads[1]*blocks[1])
+    ny = Int(threads[2]*blocks[2])
 
     # derived numerics
     dx, dy  = lx / nx, ly / ny
-    xc, yc  = LinRange(dx / 2, lx - dx / 2, nx), LinRange(dy / 2, ly - dy / 2, ny)
+    xc, yc  = LinRange(dx / 2, lx - dx / 2, nx), LinRange(dy / 2, ly - dy / 2, ny)#??
     θ_dτ    = max(lx, ly) / re / cfl / min(dx, dy)
     β_dτ    = (re * k_ηf) / (cfl * min(dx, dy) * max(lx, ly))
-    # array initialisation
-    Pf      = CuArray(@. exp(-(xc - lx / 2)^2 - (yc' - ly / 2)^2))
-    qDx     = CUDA.zeros(Float64, nx + 1, ny)
-    qDy     = CUDA.zeros(Float64, nx, ny + 1)
-    r_Pf    = CUDA.zeros(nx, ny)
+
+    if useThreads==false
+        # GPU array initialisation
+        Pf = CuArray(@. exp(-(xc - lx / 2)^2 - (yc' - ly / 2)^2))
+        
+        qDx     = CUDA.zeros(Float64, nx + 1, ny)
+        qDy     = CUDA.zeros(Float64, nx, ny + 1)
+        r_Pf    = CUDA.zeros(nx, ny)
+        
+    else#CPU array initialization
+        Pf      = @. exp(-(xc - lx / 2)^2 - (yc' - ly / 2)^2)
+        qDx     = zeros(Float64, nx + 1, ny)
+        qDy     = zeros(Float64, nx, ny + 1)
+        r_Pf    = zeros(nx, ny)
+    end
+        
     #precompute divisions
     k_ηf_dx, k_ηf_dy = k_ηf/dx, k_ηf/dy
     _1_θ_dτ = 1.0./(1.0 + θ_dτ)
@@ -118,8 +140,11 @@ function Pf_diffusion_2D(nx, ny , maxiter;do_check = false, useThreads = false)
             t_tic = Base.time()
         end
 
-        @CUDA blocks blocks=blocks threads=threads compute!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ, _β_dτ_dx, _β_dτ_dy, useThreads)
-        synchronize()
+        if useThreads
+            compute_t!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ, _β_dτ_dx, _β_dτ_dy)
+        else
+            compute!(nx, ny, qDx, qDy, Pf, k_ηf_dx, k_ηf_dy, _1_θ_dτ, _β_dτ_dx, _β_dτ_dy, threads, blocks)
+        end
        
         if do_check && iter % ncheck == 0
             r_Pf .= diff(qDx, dims=1) ./ dx .+ diff(qDy, dims=2) ./ dy
@@ -145,7 +170,7 @@ function Pf_diffusion_2D(nx, ny , maxiter;do_check = false, useThreads = false)
     Aeff = 2*3 * nx*ny * 8 / 1e9 #2 = read and write, 3 = amount of arrays, nx"ny gridsize, *8/1e9 convert to Gb
     Teff = Aeff / t_it
     @printf("Time = %1.3f sec, Aeff = %1.3f GB, Teff = %1.3f GB/s \n", t_it * niter, Aeff, Teff)
-    return Teff, Array(Pf)
+    return Array(Pf)
 end
 
 nx = ny = 127
@@ -154,3 +179,4 @@ Pf_gpu = Pf_diffusion_2D(nx, ny, maxiter, do_check = true, useThreads = false)
 Pf_cpu = Pf_diffusion_2D(nx, ny, maxiter, do_check = true, useThreads = true)
 
 print("max error: $(maximum(Pf_gpu - Pf_cpu))")
+flush(stdout)
--- homework-7/Readme.md
+++ homework-7/Readme.md
@@ -1,3 +1,6 @@
 Exercise 1:  See lecture7_ex1.ipynb
 Value from Task 7
 My diffusion solver reaches about 32 % of the GPU’s peak memory bandwidth, meaning it performs well but could, in theory, be up to ~3× faster if memory access were perfectly optimized.
+
+Exercise 2:
+The maximal error from the gpu and cpu calculation was 8.326672684688674e-17, which is machine precision 
